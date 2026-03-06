d424909fa369036ed67f683a9b59d5e9fd6d79c2
adimael <adimaelbr@gmail.com>
2026-03-06 19:03:39 -0300

    fix: Remove o plugin de e-mail

--- src/Modules/Email/src/Database/Seeders/EmailSeeder.php
+++
@@ -1,7 +0,0 @@
-<?php
-return function (PDO $db) {
-    $db->exec("
-        INSERT INTO emails (to_email, subject, body)
-        VALUES ('demo@example.com', 'Bem-vindo', '<p>Bem-vindo à plataforma.</p>')
-    ");
-};
